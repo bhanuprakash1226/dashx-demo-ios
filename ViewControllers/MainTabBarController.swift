651e4f848ec493850dbdb8ce076e95c870666690
Naidu Uppada <appalanaidusravanuppada@gmail.com>
2022-07-28 08:00:57 +0530

    SDK-170 Demo App: Tab bar: Increase the height
    - Added shadow to show separation of Content from TabBar since increasing the tab bar need some customization

--- ViewControllers/MainTabBarController.swift
+++ ViewControllers/MainTabBarController.swift
@@ -16,6 +16,18 @@ class MainTabBarController: UITabBarController {
         super.viewDidLoad()
         DashXUtils.trackEventClient()
         DashXUtils.client1.track("TestEventFromiOSApp")
+        setShadowForTaBar()
+    }
+    
+    override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+        setShadowForTaBar()
+    }
+    
+    func setShadowForTaBar() {
+        tabBar.layer.shadowOffset = CGSize(width: 0, height: 0)
+        tabBar.layer.shadowRadius = 10
+        tabBar.layer.shadowColor = UIColor(named: "secondaryColorDisabled")?.cgColor
+        tabBar.layer.shadowOpacity = 0.5
     }
     
 }
